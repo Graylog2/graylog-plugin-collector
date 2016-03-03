e34fe8b013632129177eb8c6a4f55c1e2c31b358
Bernd Ahlers <bernd@graylog.com>
2016-03-03 13:37:41 +0100

    Change collectors URL in UI to /system/collectors

--- src/web/index.jsx
+++ src/web/index.jsx
@@ -6,22 +6,15 @@ import ConfigurationsPage from './ConfigurationsPage';
 import CollectorConfiguration from './CollectorConfiguration';
 
 PluginStore.register(new PluginManifest(packageJson, {
-  /* This is the place where you define which entities you are providing to the web interface.
-     Right now you can add routes and navigation elements to it.
-
-     Examples: */
-
-  // Adding a route to /sample, rendering YourReactComponent when called:
-
   routes: [
-      { path: '/collectors', component: CollectorsPage },
-      { path: '/collectors/configurations', component: ConfigurationsPage },
-      { path: '/collectors/configurations/:id', component: CollectorConfiguration}
+      { path: '/system/collectors', component: CollectorsPage },
+      { path: '/system/collectors/configurations', component: ConfigurationsPage },
+      { path: '/system/collectors/configurations/:id', component: CollectorConfiguration}
   ],
 
   // Adding an element to the top navigation pointing to /sample named "Sample":
 
   systemnavigation: [
-      { path: '/collectors', description: 'Collectors' },
+      { path: '/system/collectors', description: 'Collectors' },
   ]
 }));
