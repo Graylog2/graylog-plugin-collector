5996df878c6c738f93653f3394fba440c656ec10
Lennart Koopmann <lennart@graylog.com>
2017-05-17 07:37:43 -0500

    UI and UX improvements  (#55)
    * Improve navigation
    
    Have the same buttons and order or buttons on every page. This should help keeping overview of where you are in the navigation.
    
    * navigation improvements for two missing pages

--- src/web/collectors/CollectorsPage.jsx
+++ src/web/collectors/CollectorsPage.jsx
@@ -27,8 +27,12 @@ const CollectorsPage = React.createClass({
             </span>
 
             <span>
+              <LinkContainer to={Routes.pluginRoute('SYSTEM_COLLECTORS')}>
+                <Button bsStyle="info active">Overview</Button>
+              </LinkContainer>
+              &nbsp;
               <LinkContainer to={Routes.pluginRoute('SYSTEM_COLLECTORS_CONFIGURATIONS')}>
-                <Button bsStyle="info">Manage configurations</Button>
+                <Button bsStyle="info">Manage Configurations</Button>
               </LinkContainer>
             </span>
           </PageHeader>
