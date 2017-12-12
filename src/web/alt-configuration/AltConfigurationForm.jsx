4d4d2b58302e17bee345560381375fb8f3698631
Marius Sturm <marius@graylog.com>
2017-12-12 15:33:49 +0100

    Make buttons useable

--- src/web/alt-configuration/AltConfigurationForm.jsx
+++ src/web/alt-configuration/AltConfigurationForm.jsx
@@ -5,6 +5,9 @@ import { Button, Col, Row } from 'react-bootstrap';
 import AceEditor from 'react-ace';
 
 import { Input } from 'components/bootstrap';
+import history from 'util/History';
+
+import CollectorConfigurationsActions from 'configurations/CollectorConfigurationsActions';
 
 const AltConfigurationForm = React.createClass({
   propTypes: {
@@ -15,11 +18,44 @@ const AltConfigurationForm = React.createClass({
     return {
       editor: undefined,
       parseErrors: [],
+      formData: {
+        id: this.props.configuration.snippets[0].snippet_id,
+        name: this.props.configuration.snippets[0].name,
+        snippet: String(this.props.configuration.snippets[0].snippet),
+        backend: this.props.configuration.snippets[0].backend,
+      },
     };
   },
 
   _getId(prefixIdName) {
     return this.state.name !== undefined ? prefixIdName + this.state.name : prefixIdName;
+  },
+
+  _save() {
+    if (this.state.parseErrors.length === 0) {
+      CollectorConfigurationsActions.saveSnippet.triggerPromise(this.state.formData, this.props.configuration.id);
+    }
+  },
+
+  _onNameChange(event) {
+    const formData = this.state.formData;
+    formData.name = event.target.value;
+    this.setState({ formData });
+  },
+
+  _onSourceChange(value) {
+    const formData = this.state.formData;
+    formData.snippet = value;
+    this.setState({ formData });
+  },
+
+  _onSubmit(event) {
+    event.preventDefault();
+    this._save();
+  },
+
+  _onCancel() {
+    history.goBack();
   },
 
   render() {
@@ -31,7 +67,7 @@ const AltConfigurationForm = React.createClass({
                  label="Name"
                  onChange={this._onNameChange}
                  help="Configuration name."
-                 value={this.props.configuration.name} />
+                 value={this.state.formData.name} />
 
 
           <Input id="configuration-editor" label="Configuration" help="Collector configuration, see quick reference for more information.">
@@ -40,14 +76,13 @@ const AltConfigurationForm = React.createClass({
                 mode="text"
                 theme="chrome"
                 name="edit"
-                fontSize={13}
-                height="18em"
+                fontSize={14}
+                height="22em"
                 width="100%"
                 editorProps={{ $blockScrolling: 'Infinity' }}
-                value=""
-                focus
-                onLoad={this._onLoad}
+                value={this.state.formData.snippet}
                 onChange={this._onSourceChange}
+                focus
               />
             </div>
           </Input>
@@ -57,7 +92,7 @@ const AltConfigurationForm = React.createClass({
           <Col md={12}>
             <div className="form-group">
               <Button type="submit" bsStyle="primary" style={{ marginRight: 10 }}>Save</Button>
-              <Button type="button" onClick={this._onBack}>Cancel</Button>
+              <Button type="button" onClick={this._onCancel}>Cancel</Button>
             </div>
           </Col>
         </Row>
