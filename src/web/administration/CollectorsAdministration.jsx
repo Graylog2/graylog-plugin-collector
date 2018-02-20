aa710517011a37725df61411350bbd46543d4f47
Edmundo Alvarez <edmundo@graylog.com>
2018-02-20 17:13:42 +0100

    Add select all input

--- src/web/administration/CollectorsAdministration.jsx
+++ src/web/administration/CollectorsAdministration.jsx
@@ -20,11 +20,52 @@ const CollectorsAdministration = createReactClass({
     };
   },
 
-  onCollectorSelect(collectorId) {
+  componentDidUpdate() {
+    this.setSelectAllCheckboxState(this.selectAllInput, this.props.collectors, this.state.selected);
+  },
+
+  setSelectAllCheckboxState(selectAllInput, collectors, selected) {
+    const selectAllCheckbox = selectAllInput ? selectAllInput.getInputDOMNode() : undefined;
+    if (!selectAllCheckbox) {
+      return;
+    }
+    // Set the select all checkbox as indeterminate if some but not items are selected.
+    selectAllCheckbox.indeterminate = selected.length > 0 && !this.isAllSelected(collectors, selected);
+  },
+
+  handleCollectorSelect(collectorId) {
     return (event) => {
-      const newSelected = event.target.checked ? lodash.union(this.state.selected, [collectorId]) : lodash.without(this.state.selected, collectorId);
-      this.setState({ selected: newSelected });
+      const newSelection = (event.target.checked ? lodash.union(this.state.selected, [collectorId]) : lodash.without(this.state.selected, collectorId));
+      this.setState({ selected: newSelection });
     };
+  },
+
+  isAllSelected(collectors, selected) {
+    return collectors.length > 0 && collectors.length === selected.length;
+  },
+
+  toggleSelectAll(event) {
+    const newSelection = (event.target.checked ? this.props.collectors.map(collector => collector.id) : []);
+    this.setState({ selected: newSelection });
+  },
+
+  formatHeader() {
+    const { collectors } = this.props;
+    const { selected } = this.state;
+    const selectedItems = this.state.selected.length;
+
+    return (
+      <ControlledTableList.Header>
+        <Input ref={(c) => { this.selectAllInput = c; }}
+               id="select-all-checkbox"
+               type="checkbox"
+               label={selectedItems === 0 ? 'Select all' : `${selectedItems} selected`}
+               disabled={collectors.length === 0}
+               checked={this.isAllSelected(collectors, selected)}
+               onChange={this.toggleSelectAll}
+               wrapperClassName="form-group-inline" />
+      </ControlledTableList.Header>
+    );
   },
 
   formatCollector(collector) {
@@ -37,7 +78,7 @@ const CollectorsAdministration = createReactClass({
                      type="checkbox"
                      label={collector.node_id}
                      checked={this.state.selected.includes(collector.id)}
-                     onChange={this.onCollectorSelect(collector.id)} />
+                     onChange={this.handleCollectorSelect(collector.id)} />
               <span className={style.collectorId}>{collector.id}</span>
             </div>
           </Col>
@@ -61,7 +102,7 @@ const CollectorsAdministration = createReactClass({
     return (
       <div>
         <ControlledTableList>
-          <ControlledTableList.Header>Collectors</ControlledTableList.Header>
+          {this.formatHeader()}
           {formattedCollectors}
         </ControlledTableList>
       </div>
