4f25f4c869668d04586a3f9018f19b652cfa131f
Edmundo Alvarez <edmundo@graylog.com>
2018-03-02 16:43:38 +0100

    Create component to render OS icon

--- src/web/collectors/BackendIndicator.jsx
+++ src/web/collectors/BackendIndicator.jsx
@@ -2,33 +2,19 @@ import React from 'react';
 import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 
+import OperatingSystemIcon from './OperatingSystemIcon';
+
 const BackendIndicator = createReactClass({
   propTypes: {
     backend: PropTypes.string.isRequired,
     operatingSystem: PropTypes.string.isRequired,
   },
 
-  operatingSystemIcon(operatingSystem) {
-    let glyphClass = 'fa-question-circle';
-    const os = operatingSystem.trim().toLowerCase();
-    if (os.indexOf('darwin') !== -1 || os.indexOf('mac os') !== -1) {
-      glyphClass = 'fa-apple';
-    } else if (os.indexOf('linux') !== -1) {
-      glyphClass = 'fa-linux';
-    } else if (os.indexOf('win') !== -1) {
-      glyphClass = 'fa-windows';
-    }
-
-    glyphClass += ' collector-os';
-
-    return (<i className={`fa ${glyphClass}`} />);
-  },
-
   render() {
     const { backend, operatingSystem } = this.props;
     return (
       <span>
-        {this.operatingSystemIcon(operatingSystem)} {backend} on {operatingSystem}
+        <OperatingSystemIcon operatingSystem={operatingSystem} /> {backend} on {operatingSystem}
       </span>
     );
   },
---
+++ src/web/collectors/OperatingSystemIcon.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import createReactClass from 'create-react-class';
+import PropTypes from 'prop-types';
+
+const OperatingSystemIcon = createReactClass({
+  propTypes: {
+    operatingSystem: PropTypes.string.isRequired,
+  },
+
+  operatingSystemIcon(operatingSystem) {
+    let glyphClass = 'fa-question-circle';
+    const os = operatingSystem.trim().toLowerCase();
+    if (os.indexOf('darwin') !== -1 || os.indexOf('mac os') !== -1) {
+      glyphClass = 'fa-apple';
+    } else if (os.indexOf('linux') !== -1) {
+      glyphClass = 'fa-linux';
+    } else if (os.indexOf('win') !== -1) {
+      glyphClass = 'fa-windows';
+    }
+
+    glyphClass += ' collector-os';
+
+    return (<i className={`fa ${glyphClass}`} />);
+  },
+
+  render() {
+    return this.operatingSystemIcon(this.props.operatingSystem);
+  },
+});
+
+export default OperatingSystemIcon;
