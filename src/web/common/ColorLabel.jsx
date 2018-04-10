4e15d68b2263b79ebdc98fe009ac895147a45ff5
Edmundo Alvarez <edmundo@graylog.com>
2018-04-10 14:56:01 +0200

    Render text in ColorLabel
    The border and text colours will be calculated automatically depending on
    the background colour the user picked.

--- src/web/common/ColorLabel.jsx
+++ src/web/common/ColorLabel.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Label } from 'react-bootstrap';
+import d3 from 'd3';
 
 class ColorLabel extends React.Component {
   static propTypes = {
     color: PropTypes.string.isRequired,
+    text: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  };
+
+  static defaultProps = {
+    // eslint-disable-next-line react/self-closing-comp
+    text: <span>&emsp;</span>,
   };
 
   render() {
-    // eslint-disable-next-line react/self-closing-comp
-    return <Label style={{ backgroundColor: this.props.color, border: '1px solid #ccc' }}>&emsp;</Label>;
+    const backgroundColor = d3.hsl(this.props.color);
+    const borderColor = backgroundColor.darker();
+    // Use dark font on brighter backgrounds and light font in darker backgrounds
+    const textColor = backgroundColor.l > 0.6 ? d3.rgb('#333333') : d3.rgb('#FFFFFF');
+    return (
+      <Label style={{
+        backgroundColor: backgroundColor.toString(),
+        border: `1px solid ${borderColor.toString()}`,
+        color: textColor.toString(),
+      }}>
+        {this.props.text}
+      </Label>
+    );
   }
 }
 
