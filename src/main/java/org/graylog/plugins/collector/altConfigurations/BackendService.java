eebf54f13ea471ab3bc73b68c0b1bdba821db580
Edmundo Alvarez <edmundo@graylog.com>
2018-04-24 13:07:37 +0200

    Prepare BackendService for pagination support

--- src/main/java/org/graylog/plugins/collector/altConfigurations/BackendService.java
+++ src/main/java/org/graylog/plugins/collector/altConfigurations/BackendService.java
@@ -1,44 +1,40 @@
 package org.graylog.plugins.collector.altConfigurations;
 
 import com.google.common.collect.Lists;
-import org.bson.types.ObjectId;
 import org.graylog.plugins.collector.altConfigurations.rest.models.CollectorBackend;
 import org.graylog2.bindings.providers.MongoJackObjectMapperProvider;
 import org.graylog2.database.MongoConnection;
-import org.mongojack.DBCursor;
+import org.graylog2.database.PaginatedDbService;
 import org.mongojack.DBQuery;
-import org.mongojack.JacksonDBCollection;
-import org.mongojack.WriteResult;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Singleton
-public class BackendService {
+public class BackendService extends PaginatedDbService<CollectorBackend> {
     private static final String COLLECTION_NAME = "collector_backends";
-    private final JacksonDBCollection<CollectorBackend, ObjectId> dbCollection;
 
     @Inject
     public BackendService(MongoConnection mongoConnection,
                           MongoJackObjectMapperProvider mapper) {
-        dbCollection = JacksonDBCollection.wrap(
-                mongoConnection.getDatabase().getCollection(COLLECTION_NAME),
-                CollectorBackend.class,
-                ObjectId.class,
-                mapper.get());
+        super(mongoConnection, mapper, CollectorBackend.class, COLLECTION_NAME);
     }
 
     public CollectorBackend load(String id) {
-        return dbCollection.findOne(DBQuery.is("_id", id));
+        return db.findOne(DBQuery.is("_id", id));
     }
 
     public CollectorBackend loadForName(String name) {
-        return dbCollection.findOne(DBQuery.is("name", name));
+        return db.findOne(DBQuery.is("name", name));
     }
 
     public List<CollectorBackend> loadAll() {
-        return toAbstractListType(dbCollection.find());
+        try (final Stream<CollectorBackend> backendsStream = streamAll()) {
+            return toAbstractListType(backendsStream.collect(Collectors.toList()));
+        }
     }
 
     public CollectorBackend fromRequest(CollectorBackend request) {
@@ -68,15 +64,6 @@ public class BackendService {
         return collectorBackend;
     }
 
-    public CollectorBackend save(CollectorBackend backend) {
-        final WriteResult<CollectorBackend, ObjectId> result = dbCollection.save(backend);
-        return result.getSavedObject();
-    }
-
-    public int delete(String id) {
-        return dbCollection.remove(DBQuery.is("_id", id)).getN();
-    }
-
     public CollectorBackend copy(String id, String name) {
         CollectorBackend collectorBackend = load(id);
         return CollectorBackend.create(
@@ -89,10 +76,6 @@ public class BackendService {
                 collectorBackend.validationCommand(),
                 collectorBackend.defaultTemplate()
         );
-    }
-
-    private List<CollectorBackend> toAbstractListType(DBCursor<CollectorBackend> backends) {
-        return toAbstractListType(backends.toArray());
     }
 
     private List<CollectorBackend> toAbstractListType(List<CollectorBackend> backends) {
