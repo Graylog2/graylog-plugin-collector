d287190b4e8f404d2e08f603b266ada0177f25cf
Dennis Oelkers <dennis@graylog.com>
2020-10-06 13:02:48 +0200

    Reusing link component abstraction from core. (#161)

--- src/web/collectors/CollectorRow.jsx
+++ src/web/collectors/CollectorRow.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import createReactClass from 'create-react-class';
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer } from 'components/graylog/router';
 
 import { Button, Label } from 'components/graylog';
 import Routes from 'routing/Routes';
