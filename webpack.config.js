819a33d74dd8ca1618bb8bca775851a4877b8ce7
Dennis Oelkers <dennis@graylog.com>
2021-11-12 11:29:01 +0100

    Adding explicit `buffer` dependency.

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,6 @@ const PluginWebpackConfig = require('graylog-web-plugin').PluginWebpackConfig;
 const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
 const path = require('path');
 
-module.exports = new PluginWebpackConfig('org.graylog.plugins.collector.CollectorPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
+module.exports = new PluginWebpackConfig(__dirname, 'org.graylog.plugins.collector.CollectorPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
   // Here goes your additional webpack configuration.
 });
