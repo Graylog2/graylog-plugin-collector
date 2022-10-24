606b87ee5ca988c942da9040dec420b60e8ee139
Linus Pahl <linus@graylog.com>
2022-10-24 09:32:45 +0200

    Improve the way we display documentation links in page header (#212)
    * Implement new subarea navigation for collectors pages.
    
    * Renaming `SubareaNavigation` to `PageNavigation`.
    
    * Update documentation link placement for collectors pages.
    
    * Migrate `ConfigurationsPage` to functional component.
    
    * Implement new subarea navigation for remaining collectors pages.
    
    * Update documentation link position for remaining collectors pages.

--- src/web/configurations/ConfigurationsPage.jsx
+++ src/web/configurations/ConfigurationsPage.jsx
@@ -18,44 +18,37 @@ import React from 'react';
 
 import DocsHelper from 'util/DocsHelper';
 import { Row, Col, Button } from 'components/bootstrap';
-import DocumentationLink from 'components/support/DocumentationLink';
 import { DocumentTitle, PageHeader } from 'components/common';
 
 import ConfigurationsList from './ConfigurationsList';
 import CollectorsPageNavigation from '../collectors/CollectorsPageNavigation';
 
+const LifecycleMessage = () => (
+  <span>The Graylog Collector plugin is discontinued and has been superseded by the new Sidecars.</span>
+);
 
-class ConfigurationsPage extends React.Component {
-  render() {
-    const lifecycleMessage = (
-      <span>The Graylog Collector plugin is discontinued and has been superseded by the new Sidecars.</span>
-    );
+const ConfigurationsPage = () => (
+  <DocumentTitle title="Collector sidecar configurations">
+    <CollectorsPageNavigation />
+    <PageHeader title="Collector Sidecar Configurations"
+                lifecycle="legacy"
+                lifecycleMessage={<LifecycleMessage />}
+                documentationLink={{
+                  title: 'Sidecar documentation',
+                  path: DocsHelper.PAGES.COLLECTOR_SIDECAR,
+                }}>
+      <span>
+        The Collector Sidecar runs next to your favourite log collector and configures it for you. Here you can
+        manage the Sidecar configurations.
+      </span>
+    </PageHeader>
 
-    return (
-      <DocumentTitle title="Collector sidecar configurations">
-        <CollectorsPageNavigation />
-        <PageHeader title="Collector Sidecar Configurations"
-                    lifecycle="legacy"
-                    lifecycleMessage={lifecycleMessage}>
-          <span>
-            The Collector Sidecar runs next to your favourite log collector and configures it for you. Here you can
-            manage the Sidecar configurations.
-          </span>
-
-          <span>
-            Read more about the collector sidecar in the{' '}
-            <DocumentationLink page={DocsHelper.PAGES.COLLECTOR_SIDECAR} text="Graylog documentation" />.
-          </span>
-        </PageHeader>
-
-        <Row className="content">
-          <Col md={12}>
-            <ConfigurationsList />
-          </Col>
-        </Row>
-      </DocumentTitle>
-    );
-  }
-}
+    <Row className="content">
+      <Col md={12}>
+        <ConfigurationsList />
+      </Col>
+    </Row>
+  </DocumentTitle>
+);
 
 export default ConfigurationsPage;
